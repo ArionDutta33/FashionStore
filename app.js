0e245fb578a25a4f6be84999ebff2c2f49616c36
ArionDutta33 <ariondutta333@outlook.com>
2024-04-17 00:55:12 +0530

    Added animations

---
+++ app.js
@@ -0,0 +1,29 @@
+let t = gsap.timeline()
+t.from(".navbar .logo,.navbar .nav-items ", {
+    y: -100,
+    duration: 2,
+    stagger: .2
+
+})
+t.from(".left h1,.left p,.left button", {
+    x: -800,
+    duration: 2,
+    stagger: .2
+})
+t.from(".middle h1", {
+    y: 100,
+    opacity: 0,
+    stagger: .2,
+    duration: .9
+
+})
+t.from("#img1 img", {
+
+    x: 700,
+    duration: .9
+})
+t.from("#img2 img", {
+
+    x: 700,
+    duration: .9
+})
